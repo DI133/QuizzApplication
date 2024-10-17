19bec262609ff0cd5c6a1a2fc68309b5fc972674
DI133 <121186610+DI133@users.noreply.github.com>
2024-10-17 21:43:36 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QuizApplicationMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
